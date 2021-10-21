abf70fda5489bc91494325be43db7068c5a5ab2b
unknown <mrgithambo@gmail.com>
2021-10-21 09:57:29 +0300

    update

--- user/views.py
+++ user/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse
 
 # Create your views here.
 class RegisterView(CreateView):
 	form_class=UserCreationForm
 	template_name='register.html'
-	success_url='main:home'
+	success_url=reverse('main:home')
